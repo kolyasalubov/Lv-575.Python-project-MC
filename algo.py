3c5f91543c5bf0dfd10f65533489abbf09d9a68d
Ostap <ostapbod674@gmail.com>
2021-02-26 23:27:37 +0200

    Algo tasks 86a, 86b, 330 added

--- algo.py
+++ algo.py
@@ -17,16 +17,75 @@ class AlgoInterface(ABC):
         pass
 
 
-class task_NAME(AlgoInterface):
+class task_86a(AlgoInterface):
 
     def execute(self) -> None:
-        # ToDo
+        ''' input natural number N \n
+        find amount of its digits '''
+        number = int(input("Eneter number N: "))
+        # number must be natural
+        print(len(str(number)) if number > 0 else "Number is not natural")
         return None
 
     @staticmethod
     def name() -> str:
-        # Todo
-        return "NAME"
+        return "86 a)"
+
+
+class task_86b(AlgoInterface):
+
+    def execute(self) -> None:
+        ''' input natural number N \n
+         find sum of its digits '''
+        number = int(input("Eneter number N: "))
+        number_str = str(number)
+        sum_ = sum(map(int, list(number_str)))
+        print(sum_)
+
+        return None
+
+    @staticmethod
+    def name() -> str:
+        return "86 б)"
+
+
+class task_330(AlgoInterface):
+
+    def execute(self) -> None:
+        ''' input natural number N \n
+            find all "ideal" numbers that is less than N \n
+
+            "ideal" - number the sum of witch deviders(without the number itself)
+            is equal to the number'''
+
+        number = int(input("Enter number N: "))
+
+        def get_deviders(numb):
+            # complexity O(sqrt(numb))
+
+            # using set to avoid duplicates of deviders
+            deviders = {1}
+            # starting from 2 because 1 is always devider of natural number
+            for i in range(2, int(numb**0.5) + 2):
+                if numb % i == 0:
+                    deviders.add(numb/i)
+                    deviders.add(i)
+            return deviders
+
+        # general complixity of print all "ideal" numbers till number N
+        # O(n*sqrt(n)) <==> O(n^(3/2))
+        for i in range(2, number):
+            if sum(get_deviders(i)) == i:
+                print(i)
+
+        # alternative form (cons: print all values after forloop ends)
+        # print(*(i for i in range(2, number)  if sum(get_deviders(i)) == i ))
+
+        return None
+
+    @staticmethod
+    def name() -> str:
+        return "330"
 
 
 if __name__ == "__main__":
