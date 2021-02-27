406c8c5b4fd05156cd01f7b26b32490f51993698
Andriy Mruts <cotyhoroshko@gmail.com>
2021-02-27 09:37:31 +0200

    107, 243a, 243b

--- algo.py
+++ algo.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod, abstractstaticmethod
+from math import log, sqrt
 
 
 class AlgoInterface(ABC):
@@ -17,16 +18,85 @@ class AlgoInterface(ABC):
         pass
 
 
-class task_NAME(AlgoInterface):
+class task_107(AlgoInterface):
 
     def execute(self) -> None:
-        # ToDo
+        m = int(input("Enter m: "))
+        k = log(m, 4)
+
+        print("k =", int(k) if k != int(k) or k == 0 else int(k) - 1)
+
         return None
 
     @staticmethod
     def name() -> str:
-        # Todo
-        return "NAME"
+        return "107"
+
+
+class task_243a(AlgoInterface):
+
+    def execute(self) -> None:
+        n = int(input("Enter n: "))
+
+        is_numbers = False
+        sq = sqrt(n)
+        for y in range(1, int(sqrt(n)) + 1):
+            # n = x^2 + y^2
+            # x^2 = sqrt(n)^2 - y^2 = (sq + y) * (sq - y)
+            x = sqrt((sq + y) * (sq - y))
+
+            if int(x) == x:
+                if int(x) >= y:
+                    print(int(x), y)
+                    is_numbers = True
+                    break
+
+            elif abs(round(x) - x) < 0.0000000001:  # prevention of calculation errors
+                if round(x) >= y:
+                    print(round(x), y)
+                    is_numbers = True
+                    break
+
+        if not is_numbers:
+            print("This number cannot be represented as the sum of two squares")
+
+        return None
+
+    @staticmethod
+    def name() -> str:
+        return "243 а)"
+
+
+class task_243b(AlgoInterface):
+
+    def execute(self) -> None:
+        n = int(input("Enter n: "))
+
+        is_numbers = False
+        sq = sqrt(n)
+        for y in range(1, int(sqrt(n)) + 1):
+            # n = x^2 + y^2
+            # x^2 = sqrt(n)^2 - y^2 = (sq + y) * (sq - y)
+            x = sqrt((sq + y) * (sq - y))
+
+            if int(x) == x:
+                if int(x) >= y:
+                    print(int(x), y)
+                    is_numbers = True
+
+            elif abs(round(x) - x) < 0.0000000001:  # prevention of calculation errors
+                if round(x) >= y:
+                    print(round(x), y)
+                    is_numbers = True
+
+        if not is_numbers:
+            print("This number cannot be represented as the sum of two squares")
+
+        return None
+
+    @staticmethod
+    def name() -> str:
+        return "243 б)"
 
 
 if __name__ == "__main__":
