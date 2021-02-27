e7a314b651209284d0f6ec9981b4f4a35dac1c38
Andriy Mruts <cotyhoroshko@gmail.com>
2021-02-27 09:46:36 +0200

    task243 modified

--- algo.py
+++ algo.py
@@ -38,7 +38,7 @@ class task_243a(AlgoInterface):
     def execute(self) -> None:
         n = int(input("Enter n: "))
 
-        is_numbers = False
+
         sq = sqrt(n)
         for y in range(1, int(sqrt(n)) + 1):
             # n = x^2 + y^2
@@ -48,17 +48,14 @@ class task_243a(AlgoInterface):
             if int(x) == x:
                 if int(x) >= y:
                     print(int(x), y)
-                    is_numbers = True
-                    break
+                    return None
 
             elif abs(round(x) - x) < 0.0000000001:  # prevention of calculation errors
                 if round(x) >= y:
                     print(round(x), y)
-                    is_numbers = True
-                    break
+                    return None
 
-        if not is_numbers:
-            print("This number cannot be represented as the sum of two squares")
+        print("This number cannot be represented as the sum of two squares")
 
         return None
 
