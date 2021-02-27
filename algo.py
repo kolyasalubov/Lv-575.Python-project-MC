7b612afbce3737f2270936c0866f3585aa02bdf7
Iryna Yarka <iryna.yarka@gmail.com>
2021-02-27 17:58:05 +0200

    Tasks 178 а), 178 б), 554 added

--- algo.py
+++ algo.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod, abstractstaticmethod
+from math import sqrt, gcd, ceil
 
 
 class AlgoInterface(ABC):
@@ -17,16 +18,73 @@ class AlgoInterface(ABC):
         pass
 
 
-class task_NAME(AlgoInterface):
+class task_178b(AlgoInterface):
 
     def execute(self) -> None:
-        # ToDo
+        n = int(input('Enter the size of sequence:'))
+        print('Enter the elements of sequence:')
+        sequence = [int(input()) for i in range(n)]
+
+        counter = 0
+        for element in sequence:
+            if element % 3 == 0 and element % 5 != 0:
+                counter += 1
+
+        print('Result:', counter)
         return None
 
     @staticmethod
     def name() -> str:
-        # Todo
-        return "NAME"
+        return "178 б)"
+
+
+class task_178c(AlgoInterface):
+
+    def execute(self) -> None:
+        n = int(input('Enter the size of array:'))
+        print('Enter the elements of sequence:')
+        sequence = [int(input()) for i in range(n)]
+
+        counter = 0
+        for element in sequence:
+            root = sqrt(element)
+            if root ** 2 == element and root % 2 == 0:
+                counter += 1
+
+        print('Result:', counter)
+        return None
+
+    @staticmethod
+    def name() -> str:
+        return "178 в)"
+
+
+class task_554(AlgoInterface):
+
+    def execute(self) -> None:
+        """
+        Finds triples using Euclid's formula
+        (Modified to print not only primitive triples)
+        """
+        num = int(input('Enter the number:')) + 1
+        for m in range(2, ceil(sqrt(num))):
+            for n in range(1, m):
+                # m and n are coprime and not both odd
+                if gcd(m, n) == 1 and (m - n) % 2 and (m ** 2 + n ** 2) < num:
+                    a = m ** 2 - n ** 2
+                    b = 2 * m * n
+                    c = m ** 2 + n ** 2
+                    if a > b:
+                        a, b = b, a
+                    k = 1
+                    while k * c < num:
+                        print(k * a, k * b, k * c)
+                        k += 1
+        return None
+
+    @staticmethod
+    def name() -> str:
+        return "554"
 
 
 if __name__ == "__main__":
