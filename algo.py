db1fcc8ef99393e171b4613336df8f386c576519
Kavyar <karabin.ua.uzh@gmail.com>
2021-02-28 14:37:45 +0200

    Tasks 87, 226, 559 added

--- algo.py
+++ algo.py
@@ -17,16 +17,107 @@ class AlgoInterface(ABC):
         pass
 
 
-class task_NAME(AlgoInterface):
+class task_87(AlgoInterface):
 
     def execute(self) -> None:
-        # ToDo
+        print("Enter n and m:")
+        try:
+            string_n, m = input().split()
+            if string_n.isdigit() and m.isdigit():
+                sum, quantity = 0, int(m)
+                if len(string_n) > quantity:
+                    for digit in list(string_n[:len(string_n) - int(quantity) - 1:-1]):
+                        sum += int(digit)
+                    print("The sum of the last {} digits of number {} is".format(quantity, string_n), sum)
+                elif len(string_n) == quantity:
+                    for digit in string_n:
+                        sum += int(digit)
+                    print("The sum of the last {} digits of number {} is".format(quantity, string_n), sum)
+                else:
+                    print("m must be less than number of digits n")
+            else:
+                print("You've entered not natural number")
+        except ValueError:
+            print("Please enter the second value")
         return None
 
     @staticmethod
     def name() -> str:
         # Todo
-        return "NAME"
+        return "87"
+
+
+class task_226(AlgoInterface):
+
+    def execute(self) -> None:
+        import math
+
+        def lcm(a, b):
+            return (a * b) // math.gcd(a, b)
+
+        result = []
+        print("Enter n and m:")
+        try:
+            n, m = input().split()
+            if n.isdigit() and m.isdigit():
+                n, m = int(n), int(m)
+                lcm = lcm(n, m)
+                for i in range(lcm, n * m, lcm):
+                    result.append(i)
+                if not len(result):
+                    print("There are no such values")
+                else:
+                    print("All common multiples less then {}: ".format(n * m), end='')
+                    for el in result:
+                        print(el, end=', ')
+                    print()
+            else:
+                print("You've entered not natural number")
+        except ValueError:
+            print("Please enter the second value")
+        return None
+
+    @staticmethod
+    def name() -> str:
+        # Todo
+        return "226"
+
+
+class task_559(AlgoInterface):
+
+    def execute(self) -> None:
+
+        import math
+
+        # Eratosthene's sieve to get primes
+        def eratosthenes(n):
+            sieve = list(range(n + 1))
+            sieve[1] = 0
+            for i in sieve:
+                if i > 1:
+                    for j in range(i + i, len(sieve), i):
+                        sieve[j] = 0
+            sieve_without_nulls = set([x for x in sieve if x != 0])
+            return set(sieve_without_nulls)
+
+        # Mersenne numbers
+        def mersen_numbers(n):
+            return set([2 ** i - 1 for i in range(2, int(math.log(n + 1, 2)) + 1)])
+
+        print("Enter n:")
+        n = input()
+        if n.isdigit():
+            n = int(n)
+            result = list(eratosthenes(n).intersection(mersen_numbers(n)))  # Mersenne primes
+            print("Mersenne primes less than {}:".format(n), sorted(result))
+        else:
+            print("You've entered not natural number")
+        return None
+
+    @staticmethod
+    def name() -> str:
+        # Todo
+        return "559"
 
 
 if __name__ == "__main__":
